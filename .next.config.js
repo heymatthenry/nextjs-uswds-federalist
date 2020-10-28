492bb9bb7fe5f2ab042f2d086b6e4edd0c687354
Matt Henry <matt.henry@gsa.gov>
2020-10-28 13:42:57 -0400

    try a trailing slash?

--- .next.config.js
+++ .next.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  basePath: process.env.BASEURL || '/',
-  assetPrefix: process.env.BASEURL || '/',
+  basePath: process.env.BASEURL ? process.env.BASEURL + '/' : '/',
+  assetPrefix: process.env.BASEURL ? process.env.BASEURL + '/' : '/',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
