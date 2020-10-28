0f50924dc01bc21e760c3f45cb3f0cfdfffab34d
Matt Henry <matt.henry@gsa.gov>
2020-10-28 11:12:08 -0400

    try an assetPrefix?

--- .next.config.js
+++ .next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   basePath: process.env.BASEURL || '/',
+  assetPrefix: process.env.BASEURL || '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
